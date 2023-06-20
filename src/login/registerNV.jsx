c136f069dcabcfd0bb2503173ffa4e1f5157068c
AndreaDiMineo <117356538+AndreaDiMineo@users.noreply.github.com>
2023-06-20 10:55:00 +0200

    Add files via upload

---
+++ src/login/registerNV.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Register from "./register";
+import Background from "./background";
+import "./style.css";
+const RegisterNV = () => {
+  return (
+    <React.Fragment>
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+        crossorigin="anonymous"
+      />
+      <Background />
+
+      <Register />
+
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+        crossorigin="anonymous"
+      ></script>
+    </React.Fragment>
+  );
+};
+export default RegisterNV;
