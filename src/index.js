761f69b1d31cf68197647accfe5c0db9438f80c5
AndreaDiMineo <117356538+AndreaDiMineo@users.noreply.github.com>
2023-06-20 09:42:10 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,74 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./homepage/homepage";
+import Login from "./login/login";
+import Register from "./register/register";
+import SearchMuseum from "./searchmuseum/components/searchmuseum";
+import { ViewProvider } from "./searchmuseum/hooks/view-context";
+import { FilterProvider } from "./searchmuseum/hooks/filter-context";
+import Profile from "./profile/profile";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <Homepage />
+      </div>
+    ),
+  },
+  {
+    path: "/login",
+    element: (
+      <div>
+        <Login />
+      </div>
+    ),
+  },
+  {
+    path: "/register",
+    element: (
+      <div>
+        <Register />
+      </div>
+    ),
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <div>
+        <ViewProvider>
+          <FilterProvider>
+            <SearchMuseum />
+          </FilterProvider>
+        </ViewProvider>
+      </div>
+    ),
+  },
+  {
+    path: "/profile",
+    element: (
+      <div>
+        <FilterProvider>
+          <Profile />
+        </FilterProvider>
+      </div>
+    ),
+  },
+]);
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
