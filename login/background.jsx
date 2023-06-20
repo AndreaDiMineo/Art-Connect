9138aeb3c343b63fe3737834892915cda1657617
Amine BenMoussa <117638885+BenMousaAmine@users.noreply.github.com>
2023-06-20 07:47:43 +0000

    Create 9 files

---
+++ login/background.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./background.css";
+
+const Background = () => {
+    return (
+        <React.Fragment>
+            <div class="area">
+                <ul class="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </React.Fragment>
+    );
+};
+export default Background;
