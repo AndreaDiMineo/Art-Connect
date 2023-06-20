06e5db23662cee663493d8a852318307e40a8f9a
AndreaDiMineo <117356538+AndreaDiMineo@users.noreply.github.com>
2023-06-20 10:08:29 +0200

    Update Museum.jsx

--- src/searchmuseum/components/Museum.jsx
+++ src/searchmuseum/components/Museum.jsx
@@ -1,12 +1,24 @@
-import "../styles/Museum.css";
+import MuseumProfile from "../../museumprofile/museumprofile";
+import "../styles/museumStyle.css";
+import { useContext } from "react";
+import { FilterContext } from "../hooks/filter-context";
 
 const Museum = () => {
+  const { rating, category } = useContext(FilterContext);
+  const ClickProfile = () => {
+    return (
+      <div>
+        <MuseumProfile/>
+      </div>
+    )
+  }
   return (
     <div className="museumCard">
       <div className="museumCardLeft">
         <img
           className="museumImg"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Field_Museum_of_Natural_History.jpg/1280px-Field_Museum_of_Natural_History.jpg"
+          onClick={ClickProfile}
         />
       </div>
       <div className="museumCardRight">
@@ -14,7 +26,7 @@ const Museum = () => {
         <div className="museumSubtitle">
           <div className="museumDistance">
             <img
-              className="mapPin_museum"
+              className="museum_icons"
               src="https://i.ibb.co/4JWWxhz/map-pin.png"
               alt=""
             />
@@ -22,20 +34,20 @@ const Museum = () => {
           </div>
           <div className="museumType">
             <img
-              className="mapPin_museum"
-              src="https://i.ibb.co/4JWWxhz/map-pin.png"
+              className="museum_icons"
+              src="https://i.ibb.co/Lh7YZq8/grid.png"
               alt=""
             />
-            <p>Storia naturale</p>
+            <p>{category}</p>
           </div>
         </div>
         <div className="museumRating">
           <img
-            className="mapPin_museum"
-            src="https://i.ibb.co/4JWWxhz/map-pin.png"
+            className="museum_icons"
+            src="https://i.ibb.co/CsM31z2/star.png"
             alt=""
           />
-          <p>4.0</p>
+          <p>{!rating ? 4.0 : 5.0}</p>
         </div>
       </div>
     </div>
