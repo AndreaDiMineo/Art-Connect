24f9b3676b271f17e82529820be33df07d0d8c71
AndreaDiMineo <117356538+AndreaDiMineo@users.noreply.github.com>
2023-06-29 15:50:56 +0200

    Update footer.jsx

--- src/homepage/footer.jsx
+++ src/homepage/footer.jsx
@@ -21,8 +21,7 @@ const Footer = () => {
                                 <p>Accessibilità</p>
                             </nav>
                         </div>
-                    </div>
-                    <div className="social">
+                        <div className="social">
                         <h3>Seguici sui social</h3>
                         <picture>
                             <img src="https://cdn2.iconfinder.com/data/icons/black-white-social-media/32/instagram_online_social_media-256.png"/>
@@ -30,6 +29,7 @@ const Footer = () => {
                             <img src="https://cdn2.iconfinder.com/data/icons/black-white-social-media/32/youtube_online_social_media-256.png"/>
                             <img src="https://cdn2.iconfinder.com/data/icons/black-white-social-media/32/online_social_media_linked_in-256.png"/>
                         </picture>
+                    </div>
                     </div>
                     <div className="last-info">
                         <p>2023© ArtConnect All Rights Reserved</p>
