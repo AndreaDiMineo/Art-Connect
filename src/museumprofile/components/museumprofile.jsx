edca5d13972f1cba03bff7b901a7ec21206d7f1f
Andrea De Pedrini <127349395+andreadepe@users.noreply.github.com>
2023-06-30 15:46:35 +0000

    Added parameters in museum profile

--- src/museumprofile/components/museumprofile.jsx
+++ src/museumprofile/components/museumprofile.jsx
@@ -6,73 +6,87 @@ import Tickets from "./tickets";
 import { useState } from "react";
 import Show from "./show";
 
-const MuseumProfile = () => {
-    const { rating, category } = useContext(FilterContext);
-    const [list, setList] = useState(false);
-    const [show, setShow] = useState(false);
+const MuseumProfile = ({ title, km, rating, category }) => {
+  const [list, setList] = useState(false);
+  const [show, setShow] = useState(false);
 
-    const mostraBiglietti = (stato) => {
-        setList(stato)
-    }
-    const mostraMostre = (stato) => {
-        setShow(stato)
-    }
-    return(
-        <div className="card">
+  const mostraBiglietti = (stato) => {
+    setList(stato);
+  };
+  const mostraMostre = (stato) => {
+    setShow(stato);
+  };
+  return (
+    <div className="card">
+      <img
+        className="museum-img"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Field_Museum_of_Natural_History.jpg/1280px-Field_Museum_of_Natural_History.jpg"
+      />
+      <div className="museum-profile">
+        <div className="stats-btns">
+          <div className="stats">
             <img
-            className="museum-img"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Field_Museum_of_Natural_History.jpg/1280px-Field_Museum_of_Natural_History.jpg"
+              className="museum_icons"
+              src="https://i.ibb.co/4JWWxhz/map-pin.png"
+              alt=""
             />
-            <div className="museum-profile">
-                <div className="stats-btns">
-                    <div className="stats">
-                        <img
-                        className="museum_icons"
-                        src="https://i.ibb.co/4JWWxhz/map-pin.png"
-                        alt=""
-                        />
-                        <p>4km</p>
-                        <img
-                        className="museum_icons"
-                        src="https://i.ibb.co/Lh7YZq8/grid.png"
-                        alt=""
-                        />
-                        <p>{!category ? "Storia" : "Arte"}</p>
-                        <img
-                            className="museum_icons"
-                            src="https://i.ibb.co/CsM31z2/star.png"
-                            alt=""
-                        />
-                        <p>{!rating ? 4.0 : 5.0}</p>
-                    </div>
-                    <div className="btns">
-                        <button className="museum-btn">Sito web</button>
-                        <button className="museum-btn">Visualizza mostre</button>
-                        <button className="museum-btn" onClick={!list === true ? () => mostraBiglietti(true) : () => mostraBiglietti(false)}>Visualizza biglietti</button>
-                    </div>
-                </div>
-                <div className="museum-info">
-                    <h1>Field Museum of Natural History</h1>
-                    <h3>Descrizione</h3>
-                    <h3>Informazioni</h3>
-                    <p><b>Indirizzo:</b> Via San Vittore, 21, 20123, Milano</p>
-                    <p><b>Orari:</b></p>
-                    <ul>
-                        <li>Lunedì - Chiuso</li>
-                        <li>Martedì - 9:30-17:00</li>
-                        <li>Mercoledì - 9:30-17:00</li>
-                        <li>Giovedì - 9:30-17:00</li>
-                        <li>Venerdì - 9:30-17:00</li>
-                        <li>Sabato - 9:30-18:30</li>
-                        <li>Domenica - 9:30-18:30</li>
-                    </ul>
-                    <p><b>Telefono:</b> 02485551</p>
-                </div>
-            </div>
-            {!list === true ? null : <Tickets/>}
-            {!show === true ? null : <Show/>}
+            <p>{parseFloat(km).toFixed(2)}km</p>
+            <img
+              className="museum_icons"
+              src="https://i.ibb.co/Lh7YZq8/grid.png"
+              alt=""
+            />
+            <p>{category}</p>
+            <img
+              className="museum_icons"
+              src="https://i.ibb.co/CsM31z2/star.png"
+              alt=""
+            />
+            <p>{rating}</p>
+          </div>
+          <div className="btns">
+            <button className="museum-btn">Sito web</button>
+            <button className="museum-btn">Visualizza mostre</button>
+            <button
+              className="museum-btn"
+              onClick={
+                !list === true
+                  ? () => mostraBiglietti(true)
+                  : () => mostraBiglietti(false)
+              }
+            >
+              Visualizza biglietti
+            </button>
+          </div>
         </div>
-    )
-}
+        <div className="museum-info">
+          <h1>{title}</h1>
+          <h3>Descrizione</h3>
+          <h3>Informazioni</h3>
+          <p>
+            <b>Indirizzo:</b> Via San Vittore, 21, 20123, Milano
+          </p>
+          <p>
+            <b>Orari:</b>
+          </p>
+          <ul>
+            <li>Lunedì - Chiuso</li>
+            <li>Martedì - 9:30-17:00</li>
+            <li>Mercoledì - 9:30-17:00</li>
+            <li>Giovedì - 9:30-17:00</li>
+            <li>Venerdì - 9:30-17:00</li>
+            <li>Sabato - 9:30-18:30</li>
+            <li>Domenica - 9:30-18:30</li>
+          </ul>
+          <p>
+            <b>Telefono:</b> 02485551
+          </p>
+        </div>
+      </div>
+      {!list === true ? null : <Tickets />}
+      {!show === true ? null : <Show />}
+    </div>
+  );
+};
 
 export default MuseumProfile;
