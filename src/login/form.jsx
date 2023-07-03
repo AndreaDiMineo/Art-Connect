d785786a32b259dd9470007cfb2fd2c92f37dc17
AndreaDiMineo <117356538+AndreaDiMineo@users.noreply.github.com>
2023-07-03 13:53:55 +0200

    Update form.jsx

--- src/login/form.jsx
+++ src/login/form.jsx
@@ -1,174 +1,195 @@
-import React, { useContext } from "react";
+import React, { useState, useContext, useNavigate } from "react";
 import "./form.css";
 import { FuncContext } from "./context";
 import { Link } from "react-router-dom";
+import app from "../database/databaseHandler";
+
+const db = app.firestore();
+const storage = app.storage();
 
 const Form = () => {
-  const {
-    toggle,
-    submitForm,
-    register,
-    passwordNascondi,
-    passwordInfo,
-    showInfoPassword,
-    togglePass,
-    Forget,
-  } = useContext(FuncContext);
+    const [utenti, setUtenti] = useState([]);
+    const navigate = useNavigate();
+    const addData = async () => {
+        const email = document.querySelector(".emailInput").value;
+        const password = document.querySelector(".passwordInput").value;
+        const snapshot = await db.collection("Utente").get();
+        const utenteData = snapshot.docs.map((doc) => doc.data());
+        setUtenti(utenteData);
+        utenti.map((utente) => {
+            if (email === utente.email && password === utente.password) {
+                navigate("/dashboard");
+            }
+            else {
+                alert("Username e/o password sbagliato/i");
+            }
+        })
+      };
+    const {
+        passwordNascondi,
+        passwordInfo,
+        showInfoPassword,
+    } = useContext(FuncContext);
 
-  return (
-    <React.Fragment>
-      <section class="vh-100">
-        <div class="container-fluid h-custom">
-          <div class="row d-flex justify-content-center align-items-center h-100">
-            <div class="col-md-9 col-lg-6 col-xl-5">
-              <img
-                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-                class="img-fluid"
-                alt="Sample image"
-              />
-            </div>
+    return (
+        <React.Fragment>
+            <section class="vh-100">
+                <div class="container-fluid h-custom">
+                    <div class="row d-flex justify-content-center align-items-center h-100">
+                        <div class="col-md-9 col-lg-6 col-xl-5">
+                            <img
+                                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                                class="img-fluid"
+                                alt="Sample image"
+                            />
+                        </div>
 
-            <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-              <form>
-                <div class="d-flex flex-row align-items-center justify-content-center">
-                  <p className="lead fw-normal mb-0 me-3">Accedi con</p>
-                  <picture style={{ marginRight: "10px" }}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="feather feather-chrome"
-                    >
-                      <circle cx="12" cy="12" r="10"></circle>
-                      <circle cx="12" cy="12" r="4"></circle>
-                      <line x1="21.17" y1="8" x2="12" y2="8"></line>
-                      <line x1="3.95" y1="6.06" x2="8.54" y2="14"></line>
-                      <line x1="10.88" y1="21.94" x2="15.46" y2="14"></line>
-                    </svg>
-                  </picture>
+                        <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+                                    <form onSubmit={addData}>
+                                        <div class="d-flex flex-row align-items-center justify-content-center">
+                                            <p className="lead fw-normal mb-0 me-3">Sign in with</p>
+                                            <picture style={{ marginRight: "10px" }}>
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    width="24"
+                                                    height="24"
+                                                    viewBox="0 0 24 24"
+                                                    fill="none"
+                                                    stroke="currentColor"
+                                                    stroke-width="2"
+                                                    stroke-linecap="round"
+                                                    stroke-linejoin="round"
+                                                    class="feather feather-chrome"
+                                                >
+                                                    <circle cx="12" cy="12" r="10"></circle>
+                                                    <circle cx="12" cy="12" r="4"></circle>
+                                                    <line x1="21.17" y1="8" x2="12" y2="8"></line>
+                                                    <line x1="3.95" y1="6.06" x2="8.54" y2="14"></line>
+                                                    <line x1="10.88" y1="21.94" x2="15.46" y2="14"></line>
+                                                </svg>
+                                            </picture>
 
-                  <picture style={{ marginRight: "10px" }}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="feather feather-facebook"
-                    >
-                      <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
-                    </svg>
-                  </picture>
+                                            <picture style={{ marginRight: "10px" }}>
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    width="24"
+                                                    height="24"
+                                                    viewBox="0 0 24 24"
+                                                    fill="none"
+                                                    stroke="currentColor"
+                                                    stroke-width="2"
+                                                    stroke-linecap="round"
+                                                    stroke-linejoin="round"
+                                                    class="feather feather-facebook"
+                                                >
+                                                    <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
+                                                </svg>
+                                            </picture>
 
-                  <picture>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="feather feather-twitter"
-                    >
-                      <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
-                    </svg>
-                  </picture>
+                                            <picture>
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    width="24"
+                                                    height="24"
+                                                    viewBox="0 0 24 24"
+                                                    fill="none"
+                                                    stroke="currentColor"
+                                                    stroke-width="2"
+                                                    stroke-linecap="round"
+                                                    stroke-linejoin="round"
+                                                    class="feather feather-twitter"
+                                                >
+                                                    <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
+                                                </svg>
+                                            </picture>
+                                        </div>
+
+                                        <div className="divider d-flex align-items-center my-4">
+                                            <p className="text-center fw-bold mx-3 mb-0">Or</p>
+                                        </div>
+
+                                        <div className="form-outline mb-4">
+                                            <label className="form-label" htmlFor="form3Example3">
+                                                Email address
+                                            </label>
+                                            <div className="input-group mb-3">
+                                                <input
+                                                    type="email"
+                                                    class="form-control form-control-lg"
+                                                    placeholder="Enter your email"
+                                                    aria-label="Enter your email"
+                                                    aria-describedby="basic-addon2"
+                                                    className="emailInput"
+                                                />
+                                                <span className="input-group-text" id="basic-addon2">
+                                                    @example.com
+                                                </span>
+                                            </div>
+                                        </div>
+
+                                        <div className="form-outline mb-3">
+                                            <label className="form-label" htmlFor="form3Example4">
+                                                Password
+                                            </label>
+                                            <input
+                                                type="password"
+                                                id="password"
+                                                class="form-control form-control-lg"
+                                                placeholder="Enter password"
+                                                className="passwordInput"
+                                                onClick={passwordInfo}
+                                                onBlur={passwordNascondi}
+                                            />
+                                            {showInfoPassword && (
+                                                <p>
+                                                    La password deve contenere più di 8 caratteri con almeno
+                                                    una lettera maiuscola, un numero e un carattere speciale.
+                                                </p>
+                                            )}
+                                        </div>
+
+                                        <div className="d-flex justify-content-between align-items-center">
+                                            <div className="form-check mb-0">
+                                                <input
+                                                    className="form-check-input me-2"
+                                                    type="checkbox"
+                                                    value=""
+                                                    id="form2Example3"
+                                                />
+                                                <label
+                                                    className="form-check-label"
+                                                    htmlFor="form2Example3"
+                                                >
+                                                    Remember me
+                                                </label>
+                                            </div>
+                                            <Link to={"/passwordreset"} className="text-body" >
+                                            Dimenticato la password?
+                                            </Link>
+                                        </div>
+
+                                        <div className="text-center text-lg-start mt-4 pt-2">
+                                            <button
+                                                type="submit"
+                                                className="btn btn-primary btn-lg"
+                                                id="login-btn"
+                                            >
+                                                Login
+                                            </button>
+                                            <p className="small fw-bold mt-2 pt-1 mb-0">
+                                            Non hai un account?{" "}
+                                                <Link to={"/register"} className="link-danger">
+                                                    Registrati
+                                                </Link>
+                                            </p>
+                                        </div>
+                                    </form>
+                        </div>
+                    </div>
                 </div>
-
-                <div className="divider d-flex align-items-center my-4">
-                  <p className="text-center fw-bold mx-3 mb-0">Oppure</p>
-                </div>
-
-                <div className="form-outline mb-4">
-                  <label className="form-label" htmlFor="form3Example3">
-                    Indirizzo email
-                  </label>
-                  <div className="input-group mb-3">
-                    <input
-                      type="email"
-                      className="form-control form-control-lg"
-                      placeholder="Enter your email"
-                      aria-label="Enter your email"
-                      aria-describedby="basic-addon2"
-                    />
-                    <span className="input-group-text" id="basic-addon2">
-                      @example.com
-                    </span>
-                  </div>
-                </div>
-
-                <div className="form-outline mb-3">
-                  <label className="form-label" htmlFor="form3Example4">
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    id="password"
-                    className="form-control form-control-lg"
-                    placeholder="Enter password"
-                    onClick={passwordInfo}
-                    onBlur={passwordNascondi}
-                  />
-                  {showInfoPassword && (
-                    <p>
-                      La password deve contenere più di 8 caratteri con almeno
-                      una lettera maiuscola, un numero e un carattere speciale.
-                    </p>
-                  )}
-                </div>
-
-                <div className="d-flex justify-content-between align-items-center">
-                  <div className="form-check mb-0">
-                    <input
-                      className="form-check-input me-2"
-                      type="checkbox"
-                      value=""
-                      id="form2Example3"
-                    />
-                    <label className="form-check-label" htmlFor="form2Example3">
-                      Remember me
-                    </label>
-                  </div>
-                  <Link to={"/passwordreset"} className="text-body">
-                    Dimenticato la password?
-                  </Link>
-                </div>
-
-                <div className="text-center text-lg-start mt-4 pt-2">
-                  <button
-                    type="submit"
-                    className="btn btn-primary btn-lg"
-                    id="login-btn"
-                  >
-                    Accedi
-                  </button>
-                  <p className="small fw-bold mt-2 pt-1 mb-0">
-                    Non hai un account?{" "}
-                    <Link to={"/register"} className="link-danger">
-                      Registrati
-                    </Link>
-                  </p>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </section>
-    </React.Fragment>
-  );
+            </section>
+        </React.Fragment>
+    );
 };
 
 export default Form;
