abf8accf93d958f42ce277c5047de45fb19bbc60
matteo-pagotto <134050046+matteo-pagotto@users.noreply.github.com>
2023-07-04 12:41:33 +0200

    Update Cookies.jsx

--- src/Privacy/Cookies.jsx
+++ src/Privacy/Cookies.jsx
@@ -1,1 +1,28 @@
-//Da fare
+import React, { useState } from 'react';
+
+const CookiePopup = () => {
+  const [showPopup, setShowPopup] = useState(true);
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+
+  const handleDecline = () => {
+    // Aggiungi qui la logica per gestire il rifiuto dei cookies
+    setShowPopup(false);
+  };
+
+  return (
+    <div className={`cookie-popup ${showPopup ? 'show' : ''}`}>
+      <div className="cookie-popup-content">
+        <p>Questo sito utilizza i cookie per garantirti la migliore esperienza possibile.</p>
+        <div>
+          <button onClick={handleClosePopup}>Accetta</button>
+          <button onClick={handleDecline}>Rifiuta</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CookiePopup;
